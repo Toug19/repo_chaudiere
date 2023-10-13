7e48db7f10a3e4096cd0d8baa746d9081435cbac
Toug19 <toug31@gmail.com>
2023-10-13 22:02:40 +0200

    big update

--- alexa_control/python_code.py
+++ alexa_control/python_code.py
@@ -1,16 +1,30 @@
+# coding: utf-8
+
 from flask import Flask
 from flask_ask import Ask, statement, convert_errors
 import RPi.GPIO as GPIO
 import logging, os
 
 GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+device2gpio=dict()
+device2gpio["chauffe eau"]=21
+device2gpio["eau chaude"]=21
+
+device2gpio["boucle"]=16
+device2gpio["e. c. s. "]=16
+
 
 app = Flask(__name__)
 ask = Ask(app, '/')
 logging.getLogger("flask_ask").setLevel(logging.DEBUG)
 
-@ask.intent('GPIOControlIntent', mapping={'status': 'status', 'pin': 'pin'})
-def gpio_control(status, pin):
+@ask.intent('GPIOControlIntent', mapping={'status': 'status', 'device': 'device'})
+def gpio_control(status, device):
+    print("Args are:")
+    print(" device: " + device)
+    print(" status: " + status)
+    pin = device2gpio[device]
 
     try:
         pinNum = int(pin)
@@ -19,8 +33,8 @@ def gpio_control(status, pin):
 
     GPIO.setup(pinNum, GPIO.OUT)
 
-    if status in ['on', 'high']:    GPIO.output(pinNum, GPIO.HIGH)
-    if status in ['off', 'low']:    GPIO.output(pinNum, GPIO.LOW)
+    if status in ['on', 'actif']:    GPIO.output(pinNum, GPIO.HIGH)
+    if status in ['off', 'inactif']:    GPIO.output(pinNum, GPIO.LOW)
 
     return statement('Je positionne la pin {} valeur {}'.format(pin, status))
 
@@ -29,4 +43,4 @@ if __name__ == '__main__':
         verify = str(os.environ.get('ASK_VERIFY_REQUESTS', '')).lower()
         if verify == 'false':
             app.config['ASK_VERIFY_REQUESTS'] = False
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0")
