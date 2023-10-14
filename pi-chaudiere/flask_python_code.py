ac3fc82ed58953c61bbc9840be0b46b5b79e6e6d
Sebastien Da Silva <toug31@gmail.com>
2023-10-14 22:52:53 +0200

    Add Intents for chauffage

--- pi-chaudiere/flask_python_code.py
+++ pi-chaudiere/flask_python_code.py
@@ -4,9 +4,18 @@ from flask import Flask
 from flask_ask import Ask, statement, convert_errors
 import RPi.GPIO as GPIO
 import logging, os
+from crontab import CronTab
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
+
+CONSIGNE_FILE='/home/pi/repo_chaudiere/pi-chaudiere/CONSIGNE_TEMPERATURE.txt'
+
+alexa_chauffage_cron_job_comment = 'Alexa_cron_job_comment'
+alexa_chauffage_cron_job_command = 'python3 /home/pi/repo_chaudiere/pi-chaudiere/2_PLANCHER_AUTO_GPIO20-PIN38.py >> Logs_Chaudiere_2_PLANCHER.log'
+alexa_chauffage_cron_job_period = 15
+
+my_cron = CronTab(user='pi')
 
 boolean2statement = dict()
 boolean2statement[0] = 'inactif'
@@ -25,6 +34,23 @@ device2gpio["chauffage"]=20
 # set all gpio as output
 for device in device2gpio:
     GPIO.setup(device2gpio[device], GPIO.OUT)
+
+def check_if_alexa_job_exists():
+    for job in my_cron:
+        if job.comment == alexa_chauffage_cron_job_comment:
+            return True
+    return False
+
+def disable_chauffage_alexa_job():
+    for job in my_cron:
+        if job.comment == alexa_chauffage_cron_job_comment:
+            my_cron.remove(job)
+            my_cron.write()
+
+def enable_chauffage_alexa_job():
+    job = my_cron.new(command=alexa_chauffage_cron_job_command, comment=alexa_chauffage_cron_job_comment)
+    job.minute.every(alexa_chauffage_cron_job_period)
+    my_cron.write()
 
 
 app = Flask(__name__)
@@ -59,6 +85,44 @@ def gpio_status():
         previous_device_pin = device2gpio[device]
         
     return statement(returned_statement)
+
+@ask.intent('Chauffage_off')
+def chauffage_off():
+    if check_if_alexa_job_exists() == False:
+        return statement("Le chauffage était déja éteint")
+    else:
+        disable_chauffage_alexa_job()
+        GPIO.output(device2gpio["plancher"], GPIO.LOW)
+        return statement('Chauffage éteint')
+
+@ask.intent('Chauffage_on')
+def chauffage_on():
+    if check_if_alexa_job_exists() == True:
+        return statement("Le chauffage était déja allumé")
+    else:
+        enable_chauffage_alexa_job()
+        return statement('Chauffage allumé')
+    
+@ask.intent('Consigne_request')
+def consigne_request():
+    consigne_file = open(CONSIGNE_FILE, "r")
+    consigne_temperature = float(consigne_file.readline())
+    consigne_file.close()
+    if check_if_alexa_job_exists() == True:
+        return statement('La consigne de température est de {} degrés'.format(str(consigne_temperature)))
+    else:
+        return statement('La consigne de température est de {} degrés. Mais le chauffage est arrêté'.format(str(consigne_temperature)))
+
+@ask.intent('Consigne_set', mapping={'temperature': 'temperature'})
+def consigne_set(temperature):
+    consigne_temperature = float(temperature)
+    consigne_file = open(CONSIGNE_FILE, "w")
+    consigne_file.write(str(consigne_temperature) + '\n')
+    consigne_file.close()
+    if check_if_alexa_job_exists() == True:
+        return statement('La consigne de température est de {} degrés'.format(str(consigne_temperature)))
+    else:
+        return statement('La consigne de température est de {} degrés. Mais le chauffage est arrêté'.format(str(consigne_temperature)))
 
 
 if __name__ == '__main__':
