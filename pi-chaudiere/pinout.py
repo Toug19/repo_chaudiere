b07d3a74924765b50114537dd422ad2188ca581a
Seb <toug31@gmail.com>
2024-02-07 16:38:06 +0100

    add pinout file

---
+++ pi-chaudiere/pinout.py
@@ -0,0 +1,10 @@
+device2gpio=dict()
+device2gpio["chaudière"]=40
+device2gpio["chauffe eau"]=40
+device2gpio["eau chaude"]=40
+
+device2gpio["boucle"]=38
+device2gpio["e. c. s. "]=38
+
+device2gpio["plancher"]=36
+device2gpio["chauffage"]=36
